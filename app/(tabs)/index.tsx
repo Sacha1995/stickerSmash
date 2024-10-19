0268501c7b9498a5ae0f907378f96ee075122f0e
Sacha Uijlen <sacha-u@hotmail.com>
2024-10-19 14:59:36 +0100

    navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,14 @@
 import { Text, View } from "react-native";
 import { StyleSheet } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About screen
+      </Link>
     </View>
   );
 }
@@ -17,6 +21,11 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   text: {
+    color: "#fff",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
     color: "#fff",
   },
 });
