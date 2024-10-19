c4fd5d828da24af5dfd43969332415acae111914
Sacha Uijlen <sacha-u@hotmail.com>
2024-10-19 15:58:58 +0100

    image uploader;not working

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,17 +2,43 @@ import { View } from "react-native";
 import { StyleSheet } from "react-native";
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
+import { useState } from "react";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
+import * as ImagePicker from "expo-image-picker";
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(
+    undefined
+  );
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer
+          imgSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
+        <Button
+          onPress={pickImageAsync}
+          theme="primary"
+          label="Choose a photo"
+        />
         <Button label="Use this photo" />
       </View>
     </View>
