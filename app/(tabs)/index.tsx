b4435436641702d22c2abe22f698f0e7f7707f53
Sacha Uijlen <sacha-u@hotmail.com>
2024-10-29 11:27:00 +0000

    use image picker

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -14,6 +14,7 @@ export default function Index() {
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       quality: 1,
     });
